52aa7fbcae2c0ef6f001778effb09bd07ec1708b
Matej Marko <matej@breakingpoint.cz>
2026-07-29 12:12:42 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-export default nextConfig; // (nebo module.exports = nextConfig; pokud nemáš .mjs)
+export default nextConfig;
