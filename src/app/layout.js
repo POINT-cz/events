ba9358719a8cca343ae89b06228331d6dc6ab19e
Matej Marko <matej@breakingpoint.cz>
2026-09-26 11:54:54 +0200

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="cs">
-      <body className="bg-[#f4f4f4] text-black font-sans antialiased selection:bg-black selection:text-white">
+      <body className="bg-[#f4f4f4] text-black antialiased selection:bg-black selection:text-white">
         {children}
       </body>
     </html>
