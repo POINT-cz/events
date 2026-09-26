a284bb39e1fb4219cc719c6b8e313c78fe21640c
Matej Marko <matej@breakingpoint.cz>
2026-09-26 11:49:58 +0200

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,9 @@
 import './globals.css';
-import { Plus_Jakarta_Sans, JetBrains_Mono } from 'next/font/google';
-
-const googleSansLike = Plus_Jakarta_Sans({ 
-  subsets: ['latin', 'latin-ext'],
-  variable: '--font-sans',
-});
-
-const mono = JetBrains_Mono({ 
-  subsets: ['latin', 'latin-ext'],
-  variable: '--font-mono',
-});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="cs" className={`${googleSansLike.variable} ${mono.variable}`}>
-      <body className="bg-[#f4f4f4] text-black antialiased selection:bg-black selection:text-white">
+    <html lang="cs">
+      <body className="bg-[#f4f4f4] text-black font-sans antialiased selection:bg-black selection:text-white">
         {children}
       </body>
     </html>
